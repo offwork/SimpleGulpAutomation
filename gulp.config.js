da6c77ecc0646efd1f2f3ca3980af1dbebea05a2
kerem ozdemir <keremozdemir@kerem-MacBook-Pro.local>
2017-05-18 21:47:27 +0300

    inject with bower files and app files

--- gulp.config.js
+++ gulp.config.js
@@ -3,14 +3,43 @@
  */
 
 module.exports = function () {
+    var client = './src/';
+    var clientApp = client + 'app/';
+    var assets = client + 'assets/';
+
     var config = {
+        /*
+         * File Path
+         * */
         allJs: [
-            /*
-            * all js to vet
-            * */
             './src/**/*.js',
             './*.js'
-        ]
+        ],
+        client: client,
+        css: assets + '**/*.css',
+        index: client + 'index.html',
+        js: [
+            clientApp + '**/*.module.js',
+            clientApp + '**/*.js',
+            '!' + clientApp + '**/*.spec.js'
+        ],
+        /*
+        * Bower and NPM locations
+        * */
+        bower: {
+            json: require('./bower.json'),
+            directory: './bower_components/',
+            ignorePath: '../..'
+        }
+    };
+
+    config.getWiredepDefaultOptions = function() {
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
+            ignorePath: config.bower.ignorePath
+        };
+        return options;
     };
     return config;
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
     argv = require('yargs').argv,
-    $ = require('gulp-load-plugins')({lazy: true}),
-    config = require('./gulp.config')();
+    config = require('./gulp.config')(),
+    $ = require('gulp-load-plugins')({lazy: true});
 
 gulp.task('vet', function () {
     log('Analyzing source with JSHint and JSCS');
@@ -12,6 +12,27 @@ gulp.task('vet', function () {
         .pipe($.jshint())
         .pipe($.jshint.reporter('jshint-stylish', {verbose: true}))
         .pipe($.jshint.reporter('fail'));
+});
+
+gulp.task('wiredep', function () {
+    log('Wire up the bower css js and our app js into the html');
+    var options = config.getWiredepDefaultOptions();
+    var wiredep = require('wiredep').stream;
+    return gulp
+        .src(config.index)
+        .pipe(wiredep(options))
+        .pipe($.inject(gulp.src(config.js, {read: false}), {
+            ignorePath: 'src', addRootSlash: false}))
+        .pipe(gulp.dest(config.client));
+});
+
+gulp.task('inject', ['wiredep'], function () {
+    log('Wire up the app css into the html, and call wiredep ');
+    return gulp
+        .src(config.index)
+        .pipe($.inject(gulp.src(config.css, {read: false}), {
+            ignorePath: 'src', addRootSlash: false}))
+        .pipe(gulp.dest(config.client));
 });
 
 /*************************************************************************************
